7333bee0bf3391db8524910147ca798717eb8866
CelinexPutte <112859814+CelinexPutte@users.noreply.github.com>
2023-01-12 20:44:57 +0100

    Eerste mobiele versie
    Alle CSS gemaakt met het oog op mobile first. Nog geen functionaliteiten.

---
+++ main.js
@@ -0,0 +1,8 @@
+let menu = document.querySelector("nav");
+let hamburger = document.querySelector(".hamburger-menu");
+
+hamburger.addEventListener("click", menuToggle);
+
+function menuToggle() {
+	menu.classList.toggle(".nav-show");
+}
